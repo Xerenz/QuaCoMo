e7dec11d211308a7730b1b09d27d8929c13b3dbd
Jyothis P <jyothisp52@gmail.com>
2020-03-29 19:37:28 +0530

    zoom fix

--- assets/js/resutMap.js
+++ assets/js/resutMap.js
@@ -15,7 +15,7 @@ function shopmap(user_pincode, shops_coordinates) {
     latlon = pinlookup[user_pincode]
     console.log(latlon)
 
-    var map = L.map('map').setView([latlon[0], latlon[1]], 13);
+    var map = L.map('map').setView([latlon[0], latlon[1]], 12);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
