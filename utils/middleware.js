82dca898fa88fd2d3f3a4e049989c77e5f9a3ad9
Jithinksatheesh <jithinksatheesh@gmail.com>
2020-03-29 15:53:53 +0530

    middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -6,14 +6,20 @@ const middlewareObj = {};
 
 middlewareObj.isLoggedIn = function (req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  req.session.redirectTo = req.originalUrl;
+  req.session.redirectTo = req.headers.referer || req.originalUrl || req.url
   req.flash("error", "You need to be logged in first"); // add a one-time message before redirect
   res.redirect("/login");
 };
 
 middlewareObj.hasAdminPrivelages = function (req, res, next) {
-  if (req.isAuthenticated()) { if (req.user.isAdmin) return next(); }
-  req.session.redirectTo = req.originalUrl;
+  if (req.isAuthenticated()) {    
+    if (req.user.isAdmin){
+      return next()
+    }
+  }
+  console.log("outside")
+  req.session.redirectTo = req.headers.referer || req.originalUrl || req.url
+  console.log("inside login :" +  req.session.redirectTo )
   req.flash("error", "You need admin privelages."); // add a one-time message before redirect
   res.redirect("/login");
 };
