518ff99ba77e76ed65e466ce61465b43f3f44129
Naseem <naseemshahvarikodan@gmail.com>
2020-04-01 00:03:27 +0530

    Added Volunteers JSON data

--- assets/js/teamData.js
+++ assets/js/teamData.js
@@ -121,7 +121,525 @@ let peopleJson = {
             desc: "3rd Year Mechanical",
             links: []
         },
-    ]
+    ],
+
+    "Volunteers": [
+        {
+          "name": "Harikrishnan M",
+          "desc": "4th Year Civil",
+          "links": []
+        },
+        {
+          "name": "PHILIP GEOPHY PETER",
+          "desc": "4th Year Civil",
+          "links": []
+        },
+        {
+          "name": "Ahmed ismail",
+          "desc": "2nd Year Civil",
+          "links": []
+        },
+        {
+          "name": "Ann Mariya David",
+          "desc": "1st Year Civil",
+          "links": []
+        },
+        {
+          "name": "Hima Chandran",
+          "desc": "2nd Year Civil",
+          "links": []
+        },
+        {
+          "name": "ANIRUDH K M",
+          "desc": "4th Year Civil",
+          "links": []
+        },
+        {
+          "name": "Mohammed Aslam",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Athul Krishna",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Vivek E V",
+          "desc": "4th Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Abiram R",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Vaishnav",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Nandanunni",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Akarsha S Binu",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Sagar Krishna",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Amrutha Dinesh",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Naveen",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Karthika Santhosh Kumar",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Akshay Ravindran",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Muhammed Sabah",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "MOHAMMED RAHIB KT",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Jishnu Jagadeesh P I",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "AKHILA MANOHARAN",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Sujith Sasikumar",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Ajal P",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Gopika Lakshmi ",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Mahesh M V",
+          "desc": "4th Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Muhammed Aslam",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "GAUTHAMKRISHNA",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Vaishnav",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "NEERAJ JAYARAJ",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "ADARSH SURENDRAN",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Nithin B",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "DEEPESH P DEEPAK",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Thomas Jose",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Fayaz Azeem P",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Sreerag S",
+          "desc": "2nd Year Electronics and Communications",
+          "links": []
+        },
+        {
+          "name": "Anseel Ameerudheen",
+          "desc": "2nd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Abhishek P Pillai",
+          "desc": "2nd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Ahammed Jaseem A",
+          "desc": "2nd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Nandakishore Prashob",
+          "desc": "3rd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Amjad Ahammed",
+          "desc": "4th Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Hemu R Prithwi",
+          "desc": "1st Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Midhun joseph",
+          "desc": "1st Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Joel Patrick",
+          "desc": "2nd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Visakh Sunil",
+          "desc": "1st Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Rifah p p",
+          "desc": "3rd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "NIKHIL JOSE",
+          "desc": "3rd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Samuel Kuriakose",
+          "desc": "3rd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Anson M Varghese",
+          "desc": "4th Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "SABEERALI K K",
+          "desc": "2nd Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Shajaz Hashim",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Sreenand mk",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "SREEKANTH H ",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Sreehari M",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Thanuja Kaladevi Gopalakrishnan",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Sandra Santhosh ",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Sanath T",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Sanath T",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Shahabana M C",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Roshan Kadavath",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Sebin Albert",
+          "desc": "4th Year Information and Technology",
+          "links": []
+        },
+        {
+          "name": "Sanjith  sadan ",
+          "desc": "3rd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "Hrishikesh Anil",
+          "desc": "3rd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "Vivek Dani Issac",
+          "desc": "3rd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "Aadith Narayan P",
+          "desc": "1st Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "Vishnukripal t",
+          "desc": "1st Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "Aswanth K T",
+          "desc": "2nd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "MOHAMED SAHAL P M",
+          "desc": "2nd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "VARUN V",
+          "desc": "3rd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Hrishi Punnakkal",
+          "desc": "2nd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Liya Elsa Benny",
+          "desc": "1st Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "S Hari Govind",
+          "desc": "2nd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Muhammad k",
+          "desc": "1st Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "AJAYDEV M",
+          "desc": "3rd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Ajaydev M",
+          "desc": "3rd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "NANDAKISHOR P R",
+          "desc": "3rd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Alan John Ajay",
+          "desc": "3rd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Yasar Aman kurikkal ",
+          "desc": "1st Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Finu rishad",
+          "desc": "1st Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Jithin Das P",
+          "desc": "2nd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "MOHAMMED NIHAL C M",
+          "desc": "3rd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "JALIB  HASSAN ",
+          "desc": "3rd Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Sourrav P R",
+          "desc": "4th Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Thrilesh P B",
+          "desc": "3rd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Midhun P",
+          "desc": "2nd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "AKHILA MANOHARAN",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Sreeraj P Rajeendran",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Akhila Manoharan",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Gokul Krishnan G",
+          "desc": "2nd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "ANOOP P M",
+          "desc": "4th Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "ABRAHAM XAVIER",
+          "desc": "3rd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "Juliet Sebastian",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Adhil CV",
+          "desc": "2nd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Gokul Gopi",
+          "desc": "3rd Year Mechanical",
+          "links": []
+        },
+        {
+          "name": "Shyam",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Joshua Joseph",
+          "desc": "1st Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Akash Venugopal",
+          "desc": "1st Year Safety and Fire",
+          "links": []
+        },
+        {
+          "name": "Sanjay kuthoor",
+          "desc": "2nd Year Electronics and Communications",
+          "links": []
+        },
+        {
+          "name": "MUHAMMED IRSHAD A J",
+          "desc": "2nd Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Cinedin Salim",
+          "desc": "4th Year Electrical and Electronics",
+          "links": []
+        },
+        {
+          "name": "Rohit P S",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "Alan joseph saji",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        },
+        {
+          "name": "ASWANTH T P",
+          "desc": "2nd Year Computer Science",
+          "links": []
+        }
+      ]
 
 }
 
